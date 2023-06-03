45520f3685208a9858e1c7b118924e38584a2e6b
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-06-03 22:12:33 +0500

    index page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,33 @@
-import React from "react"
+import React, { useState } from 'react'
+import HeroSection from '../components/HeroSection'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import InfoSection from '../components/InfoSection'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
+import Services from '../components/Services'
+import Footer from '../components/Footer'
 
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        let abortController = new AbortController();
+        setIsOpen(!isOpen);
+        return () => {
+            abortController.abort();
+        }
+    }
 
     return (
         <>
-     <h1>index page</h1>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+            <HeroSection/>
+            <InfoSection {...homeObjOne}/>
+            <InfoSection {...homeObjTwo}/>
+            <Services/>
+            <InfoSection {...homeObjThree}/>
+            <Footer/>
         </>
     )
 }
