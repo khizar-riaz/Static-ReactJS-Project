1e57f8045096ece2128826535368b5a9f4abef6d
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-06-05 11:05:35 +0500

    services component

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,11 +1,40 @@
 import React from 'react'
-const Services = () => {
+import Icon1 from '../../images/svg-1.svg'
+import Icon2 from '../../images/svg-2.svg'
+import Icon3 from '../../images/svg-3.svg'
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesCard,
+  ServicesIcon,
+  ServicesH2,
+  ServicesP
+} from './ServicesElements'
 
+const Services = () => {
   return (
-    <>
-    <h1>services</h1>
-  </>
+    <ServicesContainer id='services'>
+        <ServicesH1> Our Services </ServicesH1>
+        <ServicesWrapper>
+            <ServicesCard>
+              <ServicesIcon src={Icon1}/>
+              <ServicesH2> Learn Khiz </ServicesH2>
+              <ServicesP> We guide users to achieve expertise in Khiz training. </ServicesP>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon2}/>
+              <ServicesH2> Virtual Offices </ServicesH2>
+              <ServicesP> You can access our platform online anywhere in the world. </ServicesP>
+            </ServicesCard>
+            <ServicesCard>
+              <ServicesIcon src={Icon3}/>
+              <ServicesH2> Premium Benefits </ServicesH2>
+              <ServicesP> Unlock our special membership card that grant access to additional content. </ServicesP>
+            </ServicesCard>
+        </ServicesWrapper>
+    </ServicesContainer>
   )
-  }
+}
 
 export default Services
