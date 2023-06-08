4b48a968a5e4d7cd7bc3b18a401e45d2389c57f6
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-06-08 19:42:41 +0500

    signin component

---
+++ src/components/Signin/SigninElements.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const Container = styled.div`
+   
+`
+
+export const FormWrap = styled.div`
+   
+`
+
+export const Icon = styled(Link)`
+  
+`
+
+export const FormContent = styled.div`
+    
+`
+
+export const Form = styled.form`
+    
+`
+
+export const FormH1 = styled.h1`
+   
+`
+
+export const FormLabel = styled.label`
+  
+`
+
+export const FormInput = styled.input`
+  
+`
+
+export const FormButton = styled.button`
+   
+`
+
+export const Text = styled.span`
+   
+`
